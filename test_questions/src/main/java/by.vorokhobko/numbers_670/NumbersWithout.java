7d6b50087d9941b37a3f489f14324fe4f68b3896
Evgeny Vorokhobko <vorokhobko2011@yandex.ru>
2018-01-30 07:04:53 +0300

    update test

--- test_questions/src/main/java/by.vorokhobko/numbers_670/NumbersWithout.java
+++ test_questions/src/main/java/by.vorokhobko/numbers_670/NumbersWithout.java
@@ -4,32 +4,40 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * NumbersWithout.
+ *
+ * Class NumbersWithout is the solution of the problem № 670 https://acmp.ru/?main=task&id_task=670.
+ * @author Evgeny Vorokhobko (vorokhobko2011@yandex.ru).
+ * @since 23.01.2018.
+ * @version 1.
+ */
 public class NumbersWithout {
 
     private void repeatingNumber(Scanner in, PrintWriter out) {
-        String number;
-        String s = in.next();
-        ArrayList<Integer> result = new ArrayList<>();
-        for (Integer i = 0; i < 10000; i++) {
-            number = i.toString();
-            if (number.length() == 1) {
-                result.add(i);
-            } else if (number.length() == 2 && number.charAt(0) != number.charAt(1)) {
-                result.add(i);
-            } else if (number.length() == 3 && number.charAt(0) != number.charAt(1)
-                    && number.charAt(1) != number.charAt(2)
-                    && number.charAt(0) != number.charAt(2)) {
-                result.add(i);
-            } else if (number.length() == 4 && number.charAt(0) != number.charAt(1)
-                    && number.charAt(1) != number.charAt(2)
-                    && number.charAt(2) != number.charAt(3)
-                    && number.charAt(0) != number.charAt(2)
-                    && number.charAt(0) != number.charAt(3)
-                    && number.charAt(1) != number.charAt(3)) {
-                result.add(i);
-            }
+        int g = in.nextInt();
+        int index = 0;
+        int position = 1;
+        for (; index < g; position++) {
+            if (check(position))
+                index++;
         }
-        out.println(result.get(Integer.parseInt(s)));
+        out.println(position - 1);
+    }
+
+    private boolean check(int element) {
+        ArrayList<Integer> result = new ArrayList<>();
+        boolean isNeedSave = true;
+        while (element != 0) {
+            int count = element % 10;
+            if (!result.contains(count)) {
+                result.add(count);
+            } else {
+                isNeedSave = false;
+            }
+            element = element / 10;
+        }
+        return isNeedSave;
     }
 
     public void run() {
